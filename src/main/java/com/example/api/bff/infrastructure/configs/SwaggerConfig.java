a4be77e4ea69d999c08caf250fa8bf4118185c38
WallisonLucas13 <lucaswalison136@gmail.com>
2024-10-01 16:18:28 -0300

    feat: removidos arquivos inuteis

--- src/main/java/com/example/api/bff/infrastructure/configs/SwaggerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.api.bff.infrastructure.configs;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-
-}
